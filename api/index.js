2dab0a49c8c46dd9df56c36b73eeddfc2888523f
darshan09-h <darshan090704@gmail.com>
2026-01-06 14:04:18 +0530

    url added to allowed origins in backend CORS

--- api/index.js
+++ api/index.js
@@ -10,7 +10,8 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:4000",
-      "http://localhost:4000"
+      "http://localhost:4000",
+      "http://localhost:3000"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
